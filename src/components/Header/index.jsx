cea159bfc9152ecda35c46043ce023311f1f6b4a
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-10-22 17:53:50 +0000

    components: add Header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import HeaderLogo from "./Logo";
+import HeaderLinks from "./Links";
+
+export default (props) => (
+  <header css={{
+    display: "flex",
+    flexWrap: "wrap",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    maxWidth: "1600px",
+    height: "72px",
+    margin: "0 auto",
+    fontSize: ".9em",
+  }}>
+    <HeaderLogo />
+    <HeaderLinks />
+  </header>
+);
