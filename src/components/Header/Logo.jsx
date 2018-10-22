b182e49b37085e0ee6c9528bf980e36948611a55
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-10-22 17:51:54 +0000

    components: add header logo

---
+++ src/components/Header/Logo.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Link from "../Link";
+import logos from "../../assets/base64/logos.yaml";
+
+export default (props) => (
+  <Link to={ document.location.pathname === "/" ? "" : "/" }>
+    <img
+      src={ logos.bastion.logomark }
+      alt="Bastion Heart"
+      height="72"
+      width="72"
+      css={{
+        padding: "15px",
+        transition: "filter .4s ease",
+        ":hover": {
+          filter: "contrast(120%)",
+        },
+      }}
+    />
+  </Link>
+);
